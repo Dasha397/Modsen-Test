8c7a27fcc765293a9ac8e58c3e7f0e5f18154073
Daria Amelchenya <remember.me397@gmail.com>
2023-06-14 17:18:21 +0300

    feat: add fetlify file

--- react-app/src/components/MapComponent.js
+++ react-app/src/components/MapComponent.js
@@ -1,14 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import { YMaps, Map, GeolocationControl, Placemark } from 'react-yandex-maps';
+import { Container } from 'react-bootstrap';
 
 const MapComponent = () => {
-	const [center, setCenter] = useState([150.751574, 137.573856]);
+	const [center, setCenter] = useState([53, 30]);
 
 	useEffect(() => {
 		if (navigator.geolocation) {
 			navigator.geolocation.getCurrentPosition(
 				(position) => {
 					const { latitude, longitude } = position.coords;
+					console.log(position.coords)
 					setCenter([latitude, longitude]);
 				},
 				(error) => {
@@ -19,17 +21,19 @@ const MapComponent = () => {
 	}, []);
 
 	return (
-		<YMaps apiKey="fd616ce8-3e0f-4d5f-9bfe-0b8b6d42a997">
-			<div style={{ width: '800px', height: '600px' }}>
-				<Map defaultState={{ center, zoom: 13 }} width="100%" height="100%" >
-					<GeolocationControl options={{ float: 'left' }} />
-					<Placemark geometry={center}
-						options={{
-							iconColor: '#FF0000',
-						}} />
-				</Map>
-			</div>
-		</YMaps>
+		<Container className='d-flex justify-content-center align-items-center'>
+			<YMaps apiKey="fd616ce8-3e0f-4d5f-9bfe-0b8b6d42a997">
+				<div style={{ width: '100%', height: '600px' }}>
+					<Map defaultState={{ center, zoom: 13 }} width="100%" height="100%" >
+						<GeolocationControl options={{ float: 'left' }} />
+						<Placemark geometry={center}
+							options={{
+								iconColor: '#FF0000',
+							}} />
+					</Map>
+				</div>
+			</YMaps>
+		</Container>
 	);
 };
 
