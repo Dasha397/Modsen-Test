3f51ebfc3513f78b433a0a6ca7b364c8057f13e6
Daria Amelchenya <remember.me397@gmail.com>
2023-06-19 22:56:30 +0300

    feat: add markers

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,11 +1,15 @@
-import PageMap from "./pages/PageMap";
+import { YMaps } from "@pbe/react-yandex-maps";
+import PageMap from "./pages/PageMap.jsx";
 import React from "react";
+import MapComponent from "./components/MapComponent.jsx";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <PageMap />
+        {/* <YMaps query={{ load: "package.full", lang: 'en_RU' }}> <PageMap /> </YMaps> */}
+        <MapComponent />
+
       </header>
     </div>
   );
